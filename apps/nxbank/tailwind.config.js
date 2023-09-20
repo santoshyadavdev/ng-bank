d160a00d34198e73c756749568c76a9c063e5144
Mathis Vester <m.vester@forstify.de>
2023-09-20 08:41:55 +0200

    feat: install and migrate to tailwindcss

---
+++ apps/nxbank/tailwind.config.js
@@ -0,0 +1,21 @@
+const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
+const { join } = require('path');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {
+      screens: {
+        sm: '600px',
+        md: '960px',
+        lg: '1280px',
+        xl: '1920px',
+      },
+    },
+  },
+  plugins: [],
+};
