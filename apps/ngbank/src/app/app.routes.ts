0669bb7dfe8d8a02719ed7eb6b99de9fbe19abb9
Mathis Vester <m.vester@forstify.de>
2023-10-13 16:35:54 +0200

    chore: reset files to origin

--- apps/ngbank/src/app/app.routes.ts
+++ apps/ngbank/src/app/app.routes.ts
@@ -1,9 +1,3 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [
-  {
-    path: 'login',
-    loadComponent: () =>
-      import('@ngbank/user').then((mod) => mod.LoginComponent),
-  },
-];
+export const appRoutes: Route[] = [];
