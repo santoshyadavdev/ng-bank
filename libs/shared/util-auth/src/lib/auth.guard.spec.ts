ab58571939a3c1b2e6f25123f332aa59618662ce
Mathis Vester <m.vester@forstify.de>
2023-10-15 23:33:51 +0200

    feat: rename guard and move to own lib

--- libs/shared/util-auth/src/lib/auth.guard.spec.ts
+++ libs/shared/util-auth/src/lib/auth.guard.spec.ts
@@ -1,11 +1,10 @@
 import { TestBed } from '@angular/core/testing';
 import { CanActivateFn } from '@angular/router';
+import { authGuard } from './auth.guard';
 
-import { userGuard } from './user.guard';
-
-describe('userGuard', () => {
+describe('authGuard', () => {
   const executeGuard: CanActivateFn = (...guardParameters) =>
-    TestBed.runInInjectionContext(() => userGuard(...guardParameters));
+    TestBed.runInInjectionContext(() => authGuard(...guardParameters));
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
