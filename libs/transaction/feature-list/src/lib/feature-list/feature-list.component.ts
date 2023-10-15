b88eafae95ad865a3c594717f6834abd3365f66b
Mathis Vester <m.vester@forstify.de>
2023-10-15 13:26:40 +0200

    feat: add feature-list lib

---
+++ libs/transaction/feature-list/src/lib/feature-list/feature-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ngbank-feature-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './feature-list.component.html',
+  styleUrls: ['./feature-list.component.css'],
+})
+export class FeatureListComponent {}
---
+++ libs/transaction/feature-list/src/lib/lib.routes.ts
@@ -0,0 +1,6 @@
+import { Route } from '@angular/router';
+import { FeatureListComponent } from './feature-list/feature-list.component';
+
+export const featureListRoutes: Route[] = [
+  { path: '', component: FeatureListComponent },
+];
