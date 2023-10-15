f925f17203248c57b608dc3849347787f744f7bf
Mathis Vester <m.vester@forstify.de>
2023-10-15 23:38:01 +0200

    feat: add guard

--- libs/transaction/feature-list/src/lib/lib.routes.ts
+++ libs/transaction/feature-list/src/lib/lib.routes.ts
@@ -1,11 +1,13 @@
 import { Route } from '@angular/router';
 import { FeatureListComponent } from './feature-list/feature-list.component';
 import { provideDomain } from '@ngbank/data-access';
+import { authGuard } from '@ngbank/util-auth';
 
 export const featureListRoutes: Route[] = [
   {
     path: 'transactions',
     component: FeatureListComponent,
     providers: [provideDomain()],
-  }, // todo: add guard
+    canActivate: [authGuard],
+  },
 ];
