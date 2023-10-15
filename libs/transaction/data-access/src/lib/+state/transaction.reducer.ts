eb8171c22c0d6fdcf651771737939c8c11113e8f
Mathis Vester <m.vester@forstify.de>
2023-10-15 17:12:13 +0200

    feat: add reducer logic and update list template

--- libs/transaction/data-access/src/lib/+state/transaction.reducer.ts
+++ libs/transaction/data-access/src/lib/+state/transaction.reducer.ts
@@ -1,15 +1,40 @@
-import { createFeature, createReducer } from '@ngrx/store';
+import { createFeature, createReducer, on } from '@ngrx/store';
 import { Transaction } from '../entities/transaction';
+import { TransactionApiActions } from './transaction.actions';
+import { HttpErrorResponse } from '@angular/common/http';
 
 export interface TransactionState {
   transactions: Transaction[];
+  error: HttpErrorResponse | null;
 }
 
 export const initialState: TransactionState = {
   transactions: [],
+  error: null,
 };
 
 export const transactionFeature = createFeature({
   name: 'transaction',
-  reducer: createReducer(initialState),
+  reducer: createReducer(
+    initialState,
+    on(
+      TransactionApiActions.transactionsLoadedSuccess,
+      (state: TransactionState, { transactions }) => ({
+        ...state,
+        transactions,
+      })
+    ),
+    on(
+      TransactionApiActions.transactionCreatedSuccess,
+      (state: TransactionState, { transaction }) => ({
+        ...state,
+        transactions: [...state.transactions, transaction],
+      })
+    ),
+    on(
+      TransactionApiActions.transactionsLoadedFailure,
+      TransactionApiActions.transactionCreatedFailure,
+      (state: TransactionState, { error }) => ({ ...state, error })
+    )
+  ),
 });
